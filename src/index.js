ab93ec7281b981fab78a3f02f582f968dc2d063f
tood21 <tood2111@gmail.com>
2023-07-02 19:23:29 +0900

    Update: react-snap 설정

--- src/index.js
+++ src/index.js
@@ -5,12 +5,28 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import {BrowserRouter} from "react-router-dom";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
-);
+const rootElement = document.getElementById('root');
+
+if (rootElement.hasChildNodes()) {
+    const root = ReactDOM.hydrateRoot(rootElement,
+        <BrowserRouter>
+            <App />
+        </BrowserRouter>
+    );
+    root.render(
+        <BrowserRouter>
+            <App />
+        </BrowserRouter>
+    );
+} else {
+    const root = ReactDOM.createRoot(rootElement);
+    root.render(
+        <BrowserRouter>
+            <App />
+        </BrowserRouter>
+    );
+}
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
